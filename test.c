c6950ca45134b3fd3ca7724f3584ce2372fc8f74
Neo Sekaleli <neosekaleli@gmail.com>
2023-09-22 15:39:21 +0200

    task_4

---
+++ test.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// This is the _printf function from your code.
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else
+            {
+                write(1, "%", 1);
+                write(1, format, 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(1, format, 1);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+}
+
